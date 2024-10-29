572f7ee8ba6bbeafeea38609aa2f61e7c41d3cc6
Raphael Braga Ribeiro <93213732+RaphaelBRibeiro@users.noreply.github.com>
2024-10-29 18:53:57 -0300

    Add files via upload

---
+++ Atividade 01/Atividade01.js
@@ -0,0 +1,8 @@
+//Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+let dia = prompt('Informe o dia da semana (em minusculo e sem acento) ');
+
+if (dia === 'sabado' || dia === 'domingo'){
+    alert ('Bom final de Semana!!!');
+} else {
+    alert ('Boa Semana! ');
+}
