af2f709122ce32cae84e1cf3dce52abe5e6ea1ef
Raphael Braga Ribeiro <93213732+RaphaelBRibeiro@users.noreply.github.com>
2024-10-29 19:02:37 -0300

    Add files via upload

---
+++ Atividade 02 - Pontuação/Atividade02.js
@@ -0,0 +1,9 @@
+//Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+
+let Pontuacao = prompt('Qual foi a pontuação?');
+
+if (Pontuacao >= 100){
+    alert ('Parabens, você ganhou!!');
+} else {
+    alert ('Tente novamente para ganhar');;
+}
