5ba857154e7dbf52aef1a56be7933f13d61dfd4f
Raphael Braga Ribeiro <93213732+RaphaelBRibeiro@users.noreply.github.com>
2024-10-29 19:02:58 -0300

    Add files via upload

---
+++ Atividade 03 - Saldo/Atividade03.js
@@ -0,0 +1,5 @@
+//Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+let Saldo = (155.44);
+alert ('Seu saldo é de R$'+ Saldo);
+
